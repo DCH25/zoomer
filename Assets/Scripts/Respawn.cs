4a0b6ef415e8085e10a36a249aab62e2567debb2
Daniel Chua <53444908+DCH25@users.noreply.github.com>
2020-09-07 14:19:39 -0400

    Respawning + Death Transition

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{   
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        Physics.SyncTransforms();
+        player.transform.position = spawnPoint.transform.position;
+    }
+}
